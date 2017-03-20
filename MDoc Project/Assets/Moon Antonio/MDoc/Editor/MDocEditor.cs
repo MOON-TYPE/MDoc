fe5d3658f760cef83f80850d904fc15d9c4ce1af
MoonAntonio <pinchodev91@gmail.com>
2017-03-20 02:20:53 +0100

    Add MDocEditor.cs

---
+++ MDoc Project/Assets/Moon Antonio/MDoc/Editor/MDocEditor.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// MDocEditor.cs (20/03/2017)													\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Herramienta para crear una documentacion simple.			\\
+// Fecha Mod:		20/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using UnityEditor;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Herramienta para crear una documentacion simple</para>
+	/// </summary>
+	public class MDocEditor : EditorWindow
+	{
+
+	}
+}
