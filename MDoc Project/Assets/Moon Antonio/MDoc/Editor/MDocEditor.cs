ed5214dc5d4c794c7301f5a8aec0f0a2a3fd2ec2
MoonAntonio <pinchodev91@gmail.com>
2017-03-20 02:32:02 +0100

    Implementado menu

--- MDoc Project/Assets/Moon Antonio/MDoc/Editor/MDocEditor.cs
+++ MDoc Project/Assets/Moon Antonio/MDoc/Editor/MDocEditor.cs
@@ -19,6 +19,16 @@ namespace MoonAntonio
 	/// </summary>
 	public class MDocEditor : EditorWindow
 	{
+		#region Menu
+		[MenuItem("Moon Antonio/MDoc", false, 0)]
+		static void InitMDoc()
+		{
+			EditorWindow window = GetWindow<MDocEditor>(true, "MDoc", true);
+			window.maxSize = new Vector2(500, 500);
+			window.minSize = new Vector2(500, 500);
 
+			window.Show();
+		}
+		#endregion
 	}
 }
